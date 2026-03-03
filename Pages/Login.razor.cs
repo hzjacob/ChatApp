b14a3cd6a69f1c2286436c7f1788a195200ea886
hzjacob <ktogrul17@gmail.com>
2026-03-03 16:22:04 +0400

    Login added wo JWT

--- Pages/Login.razor.cs
+++ Pages/Login.razor.cs
@@ -1,3 +1,4 @@
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.Web;
@@ -9,27 +10,45 @@ namespace ChatApp.Pages
 {
     public partial class Login
     {
-        private string? username;
+        private string? email;
         private string? password;
         InputType PasswordInput = InputType.Password;
         [Inject] NavigationManager Navigation { get; set; } = null!;
         [Inject] IJSRuntime JSRuntime { get; set; } = null!;
         [Inject] IHttpClientFactory ClientFactory { get; set; } = null!;
         
-        private void HandleLogin()
+        private async Task HandleLogin()
         {
+            var loginUser = new 
+            {
+                User_email = email,
+                Password = password
+            };
             var client = ClientFactory.CreateClient("ChatAppAPI");
-            
-            JSRuntime.InvokeVoidAsync("sessionStorage.setItem", "username", username);
 
-            Navigation.NavigateTo("/chat");
+            var request = await client.PostAsJsonAsync("api/users/login", loginUser);
+
+            if (request.IsSuccessStatusCode)
+            {
+                var response = await request.Content.ReadFromJsonAsync<UserDto>();
+                string username = response.Username;
+
+                await JSRuntime.InvokeVoidAsync("sessionStorage.setItem", "username", username);
+                Navigation.NavigateTo("/chat");
+            }
+            else
+            {
+                Navigation.NavigateTo("/Home");
+            }
+
+            
 
         }
         private async Task HandleKeyPress(KeyboardEventArgs e)
         {
             if(e.Key == "Enter")
             {
-                HandleLogin();
+                await HandleLogin();
             }
         }
         private void GoToRegister()
